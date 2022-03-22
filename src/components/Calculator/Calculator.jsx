1db81486b8a6d6d8d81c2e654edb4d1ee940172f
mu2ku <gmutuku@gmail.com>
2022-03-21 22:15:04 -0400

    commit basic addition and subtraction

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -5,8 +5,14 @@ import Keypad from '../Keypad/Keypad';
 
 const Calculator = (props) => {
 
+  const [status, setStatus] = useState()
+
   const handleClick = (evt) => {
-    props.setSum((props.sum *10)+evt)
+    if(Number.isInteger(evt)) {
+      props.setSum((props.sum *10)+evt)
+    } else {
+      setStatus(evt)
+    }
   }
 
   return ( 
@@ -14,7 +20,7 @@ const Calculator = (props) => {
       <div className={styles.wrapper}>
         <div className={styles.calculator}>
           <Screen sum={props.sum} />
-          <Keypad calcItems={props.calcItems} handleClick={handleClick} setSum={props.setSum} sum={props.sum} test={props.test} setTest={props.setTest}/>
+          <Keypad status={status} calcItems={props.calcItems} handleClick={handleClick} setSum={props.setSum} sum={props.sum} test={props.test} setTest={props.setTest}/>
         </div>
       </div>
     </>
--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,13 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../Keypad/Keypad.module.css'
 
 const Keypad = (props) => {
   
   const handleKey = (evt) => {
     let key = evt.target.innerHTML
-    if(parseInt(key)) props.handleClick(parseInt(key))
-    else if(key === "AC") props.setSum(0)
-    else if(evt.target.id === "Plus") props.setTest([props.sum,...props.test])
+    console.log(key)
+    
+    if(parseInt(key) || parseInt(key) === 0) {
+      props.handleClick(parseInt(key))
+      console.log(key)
+    } else {
+      props.handleClick(evt.target.id)
+    }
+
+    //Reset sum to 0 and test to empty array
+    if(key === "AC") {
+      props.setSum(0)
+      props.setTest([])
+    }
+    
+    if(evt.target.id === "Plus") {
+      if(props.test.length !== 0) {props.setTest([props.sum+props.test[0],...props.test])}
+      else {props.setTest([props.sum,...props.test])}
+      props.setSum(0)
+    } 
+    
+    else if(evt.target.id === "Minus"){
+      if(props.test.length !== 0) {props.setTest([props.test[0]-props.sum,...props.test])}
+      else {props.setTest([props.sum,...props.test])}
+      props.setSum(0)
+    } else if(evt.target.id === "Equals"){
+      if(props.status === "Plus") {
+        props.setSum(props.test[0]+props.sum)
+        props.setTest([props.test[0]+props.sum])
+      }
+      if(props.status === "Minus") {
+        props.setSum(props.test[0]-props.sum)
+        props.setTest([props.test[0]-props.sum])
+      }
+    }
   }
 
   return (  
